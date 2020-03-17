6c1c96abe14ccc8e7fa968a84de06b017719876d
Ewan Harris <rc0037145@student.riversidecollege.ac.uk>
2020-03-17 12:48:32 +0000

    Add project files.

---
+++ Assignment 2/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            //declare resistor values
+            double resistor1, resistor2;
+
+            //read values from textboxes
+
+
+            //for next lesson add pass code from forces 1 to use tex box
+            try
+            {
+                resistor1 = 
+                resistor2 = ;
+            }
+            catch
+            {
+                //show message when nothing is typed into the texboxes
+                MessageBox.Show("type a number in both resistor boxes!");
+                resistor1 = 0.0;
+                resistor2 = 0.0;
+            }  
+
+            //calculate the total reistance
+            double resistance = resistor1 + resistor2;
+
+            //display total resitance
+            label1.Text = " total resistance = " + resistance + "ohms";
+
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            //declare resistor values
+            double resistor3, resistor4;
+
+            //read each textbox value
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
